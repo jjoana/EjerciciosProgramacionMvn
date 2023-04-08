d1b880d5265693c298ede656b0199dde64bc31d5
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-04-08 11:38:04 +0000

    Casi terminado el Ejercicio 14 de la hoja parate expresiones lambda

---
+++ Ejercicio14/src/main/java/com/Programaciom/App.java
@@ -0,0 +1,27 @@
+package com.Programaciom;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+
+public class App {
+    public static void main(String[] args) {
+        int[] listaEnteros = {2,4,7,3,9,7,4};
+        IntStream.of(listaEnteros)
+            .forEach(System.out::println);
+
+        IntStream.range(1,50)
+            .forEach(System.out::println);
+
+        IntStream.iterate( 2, (n)-> n+2)
+            .limit(10)
+            .forEach(System.out::println);
+        
+        Random rnd = new Random();
+
+        DoubleStream doublesd = DoubleStream.empty();
+        DoubleStream.concat(doublesd, rnd.doubles())
+        .limit(10)
+        .forEach(System.out::println);
+    }
+}
